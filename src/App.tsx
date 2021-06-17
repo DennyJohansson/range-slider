e49739224a6971f4f289aeea56836688e5f6a7e2
Denny Johansson <dennybjohansson@gmail.com>
2021-06-17 21:17:28 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import RangeSlider from './RangeSlider'
+
+function App() {
+  return (
+    <RangeSlider />
+  )
+}
+
+export default App
---
+++ src/RangeSlider/RangeSlider.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RangeSlider: React.FC = () => {
+  return <div 
+    css={css`
+      color: #ece9fb;
+      &:hover,
+      &:active {
+        color: #6f3ed1;
+      }
+      `}>touch area</div>
+}
+
+export default RangeSlider
